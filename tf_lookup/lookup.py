0dde5fbb04164aeb2987f8613c44e77d81a4215e
Yibing Liu <zapplea@gmail.com>
2017-11-05 16:30:54 +1100

    add

--- tf_lookup/lookup.py
+++ tf_lookup/lookup.py
@@ -10,26 +10,25 @@ def gen():
     with g.device('/gpu:0'):
         with g.as_default():
             print('create graph')
-            t=tf.Variable(np.array(table[:2]),name='table')
+            t=tf.Variable(np.array(table),name='table')
             saver=tf.train.Saver()
         with tf.Session(graph=g,config=tf.ConfigProto(allow_soft_placement=True)) as sess:
             print('print t:')
             sess.run(tf.global_variables_initializer())
             print('run table')
-            result=sess.run(t)
-            print(type(result))
-            saver.save(sess,'lookup_model.ckpt')
+            print(sess.run(t)[:10])
+            saver.save(sess,'model/lookup_model.ckpt')
 
 
 def restore():
     g=tf.Graph()
     with g.device('/gpu:0'):
         with g.as_default():
-            saver=tf.train.import_meta_graph('lookup_model.ckpt.meta')
-            v = tf.get_tensor_by_name('table:0')
+            saver=tf.train.import_meta_graph('model/lookup_model.ckpt.meta')
+            t = g.get_tensor_by_name('table:0')
         with tf.Session(graph=g,config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-            saver.restore(sess,'lookup_model.ckpt')
-            print(sess.run(v))
+            saver.restore(sess,'model/lookup_model.ckpt')
+            print(sess.run(t)[:10])
 
 
 if __name__ == '__main__':
